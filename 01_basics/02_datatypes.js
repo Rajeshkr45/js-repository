529bb1f49c4842a9f528a15a2515355fe6f677d8
Rajeshkr45 <160626274+Rajeshkr45@users.noreply.github.com>
2024-03-17 08:53:11 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+let fullName = "Rajesh";
+let age =18;
+let isLoggedIn=false;
+
+console.log(fullName);
+
+//Different types of datatypes
+//number 
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => no value
+//symbol => unique
+
+//objects
+//typeof  null => object, undefined => undefined
+
+console.log(typeof null);   // object
+console.log(typeof undefined);   // undefined
